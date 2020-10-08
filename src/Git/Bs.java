63f7dfd0a391069a08b2137f60edfceb5d952820
shreenidhi8095 <shreenidhi.an@gmail.com>
2020-10-08 12:04:34 +0530

    modified

--- src/Git/Bs.java
+++ src/Git/Bs.java
@@ -1,5 +1,10 @@
 package Git;
 
 public class Bs {
-
+	public static void main(String[] args) {
+		String s1="nidhi";
+		for(int i=s1.length()-1;i>=0;i--) {
+			System.out.print(s1.charAt(i));
+		}
+	}
 }
